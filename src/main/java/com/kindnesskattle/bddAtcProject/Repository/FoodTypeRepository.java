753a02126125113069e70eada3b784ae5b041db6
Ajay Pratap Singh <160117701+ajaySinghBBD@users.noreply.github.com>
2024-03-12 21:04:50 +0530

    Delete src directory

--- src/main/java/com/kindnesskattle/bddAtcProject/Repository/FoodTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kindnesskattle.bddAtcProject.Repository;
-
-import com.kindnesskattle.bddAtcProject.Entities.FoodType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodTypeRepository extends JpaRepository<FoodType, Long> {
-}
